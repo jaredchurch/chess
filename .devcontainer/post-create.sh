a03a81dffa8bd4fe02bdccf03e526bf523186954
Jared <81801445+jaredchurch@users.noreply.github.com>
2026-04-28 23:07:12 +1200

    fix error on github CLI Install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -118,10 +118,15 @@ echo "✅ Done"
 
 # Installing GitHub CLI
 echo -e "\n🐙 Installing GitHub CLI..."
-run_command "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg"
-run_command 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/github-cli-archive.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null'
-run_command "sudo apt-get update"
-run_command "sudo apt-get install gh"
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo DEBIAN_FRONTEND=noninteractive apt-get update \
+	&& sudo DEBIAN_FRONTEND=noninteractive apt-get install gh -y
 echo "✅ Done"
 
 # Installing Spec Kit (Spec-Driven Development)
